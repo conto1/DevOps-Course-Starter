3edb6680ed4bdee3269eb749f2dac3585cde2d3c
Tayo Olubamibo <tayonile@gmail.com>
2024-03-20 10:13:05 +0000

    module 3 changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,43 +1,58 @@
 import os
 from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data import trello_items
+from todo_app.data import trello_items, ViewModel
+from todo_app.data.Item import Item
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-@app.route('/')
-def index():
-        return render_template('index.html', items=trello_items.get_items(), route='display')
+    @app.route('/')
+    def index():
+        #dictionary_items = trello_items.get_items()
 
-@app.route('/add', methods = ["GET","POST"])
-def add():
-    if (request.method== "GET"):
-        return render_template('index.html',statuses=trello_items.get_lists())
-    else:
-        item = {}
-        item['name'] = request.form.get('taskname')
-        item['idList'] = os.getenv("TRELLO_TODO_LIST_ID")
-        trello_items.add_item(item)
+        #item_list: list[Item] = []
+
+        #for dictionary in dictionary_items:
+            #    item = item.from_dictionary(dictionary)
+            #   item_list.append(item)
+
+        #view_model = ViewModel.ViewModel(item_list)
+        #return render_template("index.html", view_model = view_model)
+        cards = trello_items.get_items()
+        print(cards)
+        view_model = ViewModel.ViewModel(cards)
+        return render_template('index.html', viewmodel=view_model, route='display')
+
+    @app.route('/add', methods = ["GET","POST"])
+    def add():
+        if (request.method== "GET"):
+            return render_template('index.html',statuses=trello_items.get_lists())
+        else:
+            item = {}
+            item['name'] = request.form.get('taskname')
+            item['idList'] = os.getenv("TRELLO_TODO_LIST_ID")
+            trello_items.add_item(item)
+            return redirect('/')
+
+    @app.route('/delete')
+    def deletetodo():
+        id = request.values.get("id","")
+        trello_items.delete_item(id)
+        return redirect('/')  
+
+    @app.route('/complete')
+    def complete_item():
+        id = request.values.get("id","")
+        trello_items.complete_item(id)
         return redirect('/')
-    
-@app.route('/delete')
-def deletetodo():
-    id = request.values.get("id","")
-    trello_items.delete_item(id)
-    return redirect('/')  
 
-@app.route('/complete')
-def complete_item():
-    id = request.values.get("id","")
-    trello_items.complete_item(id)
-    return redirect('/')
-    
-@app.route('/start')
-def start_item():
-    id = request.values.get("id","")
-    trello_items.start_item(id)
-    return redirect('/')
+    @app.route('/start')
+    def start_item():
+       return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+    return app
